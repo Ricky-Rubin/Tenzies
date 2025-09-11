72ff4d3d8a1119940706164a8ecfb9895cfdac3d
Owolabi <owolabi1738@gmail.com>
2025-09-11 14:29:59 +0100

    Saved result of a function that outputs an array of ten random numbers between one and six in state and mapped over the state to have each number assigned to a die button. Created a new function and button to roll the dice and feed the buttons with the new numbers produced. Styled the components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
+import React from "react"
 import Die from "/src/Die.jsx"
 
 export default function App() {
+const [dieValue, setDieValue] = React.useState(generateDiceNumbers())
+
+  function generateDiceNumbers() {
+    const numbersArray = [];
+
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.floor(Math.random() * 6) + 1;
+      numbersArray.push(randomNumber);
+    }
+
+      return(numbersArray);
+  }
+
+  function rollDice() {
+    const result = generateDiceNumbers();
+    setDieValue(result);
+  }
+
+  const assignNumber = dieValue.map((dieNumber) => {
+    return <Die value={dieNumber} />
+  })
+
   return (
-    <main className="main">
-      <div className="die-box-container">
-        <Die value="5" />
-        <Die value="1" />
-        <Die value="4" />
-        <Die value="6" />
-        <Die value="4" />
-        <Die value="2" />
-        <Die value="5" />
-        <Die value="3" />
-        <Die value="1" />
-        <Die value="2" />
+    <main>
+      <div className="dice-box-container">
+        {assignNumber}
       </div>
+
+      <button className="roll-button" onClick={rollDice}>Roll Again</button>
     </main>
   )
 }
