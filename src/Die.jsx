1b3811d564672b0708d4e3a461ed7b8c85e7821c
Owolabi <owolabi1738@gmail.com>
2025-09-16 02:02:49 +0100

    Added more accessibility features

--- src/Die.jsx
+++ src/Die.jsx
@@ -3,6 +3,7 @@ export default function Die (props) {
         <button className="dieBox" 
                 style={{ backgroundColor: props.held ? "#59E391" : "white" }} 
                 onClick={props.id}
+                aria-pressed={props.held}
                 aria-label={`Die with value ${props.value}, 
                 ${props.held} ? "held" : "not held" `} >
             {props.value}
